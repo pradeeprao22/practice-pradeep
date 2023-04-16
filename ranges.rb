d5c856548567151a5db544f2f91e5a07865f74b8
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-04-16 12:39:53 +0100

    ranges structure add

---
+++ ranges.rb
@@ -0,0 +1,13 @@
+class Ranges
+
+    customer = ["one", "two", "three", "four", "five", "six", "seven"]
+
+    puts(customer[0]) 
+    
+    @ranges = 6..10
+
+    puts @ranges.include?(7)
+
+    puts "someids#{@ranges} "
+
+end
