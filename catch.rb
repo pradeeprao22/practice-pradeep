88655d26ddb4fd5cbee488d66e5dfa82de27a5f2
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-04-24 21:18:19 +0100

    catch practice

---
+++ catch.rb
@@ -0,0 +1,22 @@
+class Catch
+    def catch_and_throw(value)
+        puts value
+
+        a = readline.chomp
+
+        #using throw statement
+        throw :value_e if a == "!"
+        return a
+
+
+        catch :value_e do
+
+        # enter number
+          number = catch_and_throw("Enter the value: ")
+        end
+    end 
+end
+
+obj = Catch.new.catch_and_throw("Enter the value")
+
+puts obj
