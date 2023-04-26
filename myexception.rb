6612307a21e9f8a71f7503bfd8cf3d65e9cb1fda
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-04-26 12:27:59 +0100

    exception method

---
+++ myexception.rb
@@ -0,0 +1,17 @@
+class MyException < StandardError
+    attr_accessor :myobject
+
+    def initialize(myobject)
+        @myobject = myobject
+    end
+
+end
+
+    begin 
+
+    raise MyException.new("Any object"), "custom class"
+
+    rescue MyException => e 
+        puts e.message
+        puts e.myobject
+    end
