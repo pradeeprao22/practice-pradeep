3a66bd57f3c7912d6da8258db3e1879d039397f3
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-06-04 22:08:22 +0100

    queue practice

---
+++ queue.rb
@@ -0,0 +1,39 @@
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(element)
+    @queue.push(element)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def empty?
+    @queue.empty?
+  end
+
+  def size
+    @queue.size
+  end
+end
+
+queue = Queue.new
+
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
+
+puts queue.dequeue
+puts queue.dequeue
+
+puts queue.empty?
+puts queue.size
+
+
+
+
