e0905cbe0bcc70f8870c49559c1b541114ea7deb
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-06-07 22:45:20 +0100

    testing of block function on loops

--- loops.rb
+++ loops.rb
@@ -1,14 +1,17 @@
 class Runloops
-    a = readline.chomp
-    i = a
+    # a = readline.chomp
+    # i = a
 
-    puts a
-
+    # puts a
+  c = Proc.new do |i|   
+    puts i
     while true
       if i > 5
         puts "welcome"
-        break
+        break;
       end
     end
+  end
 
+  puts c.call(10)
 end
